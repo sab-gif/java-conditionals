a8da0a24e8996835b802e2a1eb82bce7c85d62bb
saba <sabaurrehman11@gmail.com>
2025-05-31 00:10:43 +0200

    add Weather program to display messages based on temperature input

--- Weather.java
+++ Weather.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Weather {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         //Escribe un programa que pida al usuario que ingrese la temperatura en celcius de su ciudad (La pregunta y la respuesta deberá estar en una sola línea) y según ello devuelva lo siguiente:
         //Si es bajo 0: Te estás congelando
         //Si es entre 0 y 10: Hace mucho frío 
@@ -7,6 +10,24 @@ public class Weather {
         //Si es entre 18 y 25: Parece que ha llegado el verano
         //Si es entre 26 y 35: Que calooorrrrr
         //Si es mayor a 36: Ahí no hay quien viva
+        System.out.print("Ingresa la temperatura en Celsius de tu ciudad: ");
+        int temperatura = scanner.nextInt();
+
+        if (temperatura < 0) {
+            System.out.println("Te estás congelando");
+        } else if (temperatura <= 10) {
+            System.out.println("Hace mucho frío");
+        } else if (temperatura <= 17) {
+            System.out.println("Con un abrigo estás bien");
+        } else if (temperatura <= 25) {
+            System.out.println("Parece que ha llegado el verano");
+        } else if (temperatura <= 35) {
+            System.out.println("Que calooorrrrr");
+        } else {
+            System.out.println("Ahí no hay quien viva");
+        }
+
+        scanner.close();
 
         //Imprime el resultado
     }
